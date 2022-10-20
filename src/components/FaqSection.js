e896c09235e78d511f3a9e6cf339d3b8f1cc3711
Amir Hossein Mostaan <amircodington@Amirs-MacBook-Air-2.local>
2022-10-20 12:50:40 +0330

    ServicesSection and FaqSection

---
+++ src/components/FaqSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div>
+      <h2>
+        Any questions? <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I want?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae
+            mollitia dolores iusto velit earum.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What products do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae
+            mollitia dolores iusto velit earum.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Diffrent payment methods?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae
+            mollitia dolores iusto velit earum.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae
+            mollitia dolores iusto velit earum.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
